9de765dcc0e5f53ddebf150f4c6f080e06c9811c
Thomas Sørup Bavnhøj <tbav@itu.dk>
2022-10-07 14:38:37 +0200

    Aged brie test case

--- GildedRose.Tests/ProgramTests.cs
+++ GildedRose.Tests/ProgramTests.cs
@@ -27,6 +27,20 @@ public class ProgramTests
     }
 
     [Fact]
+    public void aged_brie_after_sellin()
+    {
+
+        _items.Add(new Item { Name = "Aged Brie", SellIn = -2, Quality = 20 });
+
+
+        _program.UpdateQuality();
+        var updatedItem = _items[0];
+
+        Assert.Equal(22, updatedItem.Quality);
+
+    }
+
+    [Fact]
     public void after_due_date_degrade_twice_as_fast()
     {
         //Arrange
@@ -38,7 +52,7 @@ public class ProgramTests
         var expected = new Item { Name = "A new item", SellIn = -3, Quality = 8 };
         //Assert
 
-        Assert.Equal(8,updatedItem.Quality);
+        Assert.Equal(8, updatedItem.Quality);
     }
 
 
@@ -59,15 +73,16 @@ public class ProgramTests
             new Item { Name = "Third item", SellIn = -2, Quality = 0 }
         });
 
-        for(int i = 0; i < expected.Count(); i++){
-            Assert.Equal(expected[i].Quality,_items[i].Quality);
+        for (int i = 0; i < expected.Count(); i++)
+        {
+            Assert.Equal(expected[i].Quality, _items[i].Quality);
         }
     }
 
     [Fact]
-    public void any_item_does_not_exceed_50_in_quality_as_days_pass() 
+    public void any_item_does_not_exceed_50_in_quality_as_days_pass()
     {
-      _items = new List < Item > {
+        _items = new List<Item> {
         new Item {
           Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20
         },
@@ -103,125 +118,130 @@ public class ProgramTests
         }
       };
 
-      for(int i = 0; i < 31; i++) {
-        _program.UpdateQuality();
-        foreach(Item item in _items) {
-          bool below50 = item.Quality < 50;
-          if(item.Name.Equals("Sulfuras, Hand of Ragnaros")) {
-            Assert.False(below50);
-          } else {
-            Assert.True(below50);
-          }
+        for (int i = 0; i < 31; i++)
+        {
+            _program.UpdateQuality();
+            foreach (Item item in _items)
+            {
+                bool below50 = item.Quality < 50;
+                if (item.Name.Equals("Sulfuras, Hand of Ragnaros"))
+                {
+                    Assert.False(below50);
+                }
+                else
+                {
+                    Assert.True(below50);
+                }
+            }
         }
-      }
     }
 
     [Fact]
-    public void Sulfuras_Will_NEVER_age() 
+    public void Sulfuras_Will_NEVER_age()
     {
-      _items.Add(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 });
-      _items.Add(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 });
+        _items.Add(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 });
+        _items.Add(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 });
 
-      Item Sulfuras = _items[0];
-      Item Sulfuras2ElectricBoogaloo = _items[1];
+        Item Sulfuras = _items[0];
+        Item Sulfuras2ElectricBoogaloo = _items[1];
 
-      Assert.Equal(0, Sulfuras.SellIn);
-      Assert.Equal(-1, Sulfuras2ElectricBoogaloo.SellIn);
+        Assert.Equal(0, Sulfuras.SellIn);
+        Assert.Equal(-1, Sulfuras2ElectricBoogaloo.SellIn);
 
-      _program.UpdateQuality();
+        _program.UpdateQuality();
 
-      Assert.Equal(0, Sulfuras.SellIn);
-      Assert.Equal(-1, Sulfuras2ElectricBoogaloo.SellIn);
+        Assert.Equal(0, Sulfuras.SellIn);
+        Assert.Equal(-1, Sulfuras2ElectricBoogaloo.SellIn);
     }
 
     [Fact]
-    public void Sulfuras_Will_NEVER_degrade() 
+    public void Sulfuras_Will_NEVER_degrade()
     {
-      _items.Add(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 });
-      _items.Add(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 });
+        _items.Add(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 });
+        _items.Add(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 });
 
-      Item Sulfuras = _items[0];
-      Item Sulfuras2ElectricBoogaloo = _items[1];
+        Item Sulfuras = _items[0];
+        Item Sulfuras2ElectricBoogaloo = _items[1];
 
-      Assert.Equal(80, Sulfuras.Quality);
-      Assert.Equal(80, Sulfuras2ElectricBoogaloo.Quality);
+        Assert.Equal(80, Sulfuras.Quality);
+        Assert.Equal(80, Sulfuras2ElectricBoogaloo.Quality);
 
-      _program.UpdateQuality();
+        _program.UpdateQuality();
 
-      Assert.Equal(80, Sulfuras.Quality);
-      Assert.Equal(80, Sulfuras2ElectricBoogaloo.Quality);
+        Assert.Equal(80, Sulfuras.Quality);
+        Assert.Equal(80, Sulfuras2ElectricBoogaloo.Quality);
     }
 
     [Fact]
-    public void BackstagePasses_will_increase_by_1_when_more_than_10_days_left() 
+    public void BackstagePasses_will_increase_by_1_when_more_than_10_days_left()
     {
-      _items.Add(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 20 });
+        _items.Add(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 20 });
 
-      Item concertInFifteen = _items[0];
+        Item concertInFifteen = _items[0];
 
-      Assert.Equal(20, concertInFifteen.Quality);
+        Assert.Equal(20, concertInFifteen.Quality);
 
-      _program.UpdateQuality();
+        _program.UpdateQuality();
 
-      Assert.Equal(23, concertInFifteen.Quality);
+        Assert.Equal(23, concertInFifteen.Quality);
 
-      _program.UpdateQuality();
-      _program.UpdateQuality();
-      _program.UpdateQuality();
-      _program.UpdateQuality();
+        _program.UpdateQuality();
+        _program.UpdateQuality();
+        _program.UpdateQuality();
+        _program.UpdateQuality();
 
-      Assert.Equal(35, concertInFifteen.Quality);
+        Assert.Equal(35, concertInFifteen.Quality);
 
-      _program.UpdateQuality();
+        _program.UpdateQuality();
 
-      Assert.Equal(0, concertInFifteen.Quality);
+        Assert.Equal(0, concertInFifteen.Quality);
     }
 
     [Fact]
-    public void BackstagePasses_will_increase_by_2_when_there_is_less_than_ten_days_left() 
+    public void BackstagePasses_will_increase_by_2_when_there_is_less_than_ten_days_left()
     {
-      _items.Add(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 20 });
+        _items.Add(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 20 });
 
-      Item concertInTen = _items[0];
+        Item concertInTen = _items[0];
 
-      Assert.Equal(20, concertInTen.Quality);
+        Assert.Equal(20, concertInTen.Quality);
 
-      _program.UpdateQuality();
+        _program.UpdateQuality();
 
-      Assert.Equal(22, concertInTen.Quality);
+        Assert.Equal(22, concertInTen.Quality);
 
-      _program.UpdateQuality();
-      _program.UpdateQuality();
-      _program.UpdateQuality();
-      _program.UpdateQuality();
+        _program.UpdateQuality();
+        _program.UpdateQuality();
+        _program.UpdateQuality();
+        _program.UpdateQuality();
 
-      Assert.Equal(30, concertInTen.Quality);
+        Assert.Equal(30, concertInTen.Quality);
 
-      _program.UpdateQuality();
+        _program.UpdateQuality();
 
-      Assert.Equal(33, concertInTen.Quality);
+        Assert.Equal(33, concertInTen.Quality);
     }
 
     [Fact]
-    public void BackstagePasses_will_increase_Quality_by_3_when_less_than_five_days_left_but_become_worthless_after_the_sellby_Becomes_less_than_zero() 
+    public void BackstagePasses_will_increase_Quality_by_3_when_less_than_five_days_left_but_become_worthless_after_the_sellby_Becomes_less_than_zero()
     {
-      _items.Add(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 20 });
-      Item concertInFive = _items[0];
-      
-      Assert.Equal(20, concertInFive.Quality);
+        _items.Add(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 20 });
+        Item concertInFive = _items[0];
 
-      _program.UpdateQuality();
+        Assert.Equal(20, concertInFive.Quality);
 
-      Assert.Equal(23, concertInFive.Quality);
+        _program.UpdateQuality();
 
-      _program.UpdateQuality();
-      _program.UpdateQuality();
-      _program.UpdateQuality();
-      _program.UpdateQuality();
-      Assert.Equal(35, concertInFive.Quality);
+        Assert.Equal(23, concertInFive.Quality);
 
-      _program.UpdateQuality();
-      Assert.Equal(0, concertInFive.Quality);
+        _program.UpdateQuality();
+        _program.UpdateQuality();
+        _program.UpdateQuality();
+        _program.UpdateQuality();
+        Assert.Equal(35, concertInFive.Quality);
+
+        _program.UpdateQuality();
+        Assert.Equal(0, concertInFive.Quality);
     }
 
 

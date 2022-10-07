c6b37da008872f37fbc2d5d34de055c950638242
Lauge Milling <lm@comonnetwork.com>
2022-10-07 13:22:12 +0200

    tests

--- GildedRose.Tests/ProgramTests.cs
+++ GildedRose.Tests/ProgramTests.cs
@@ -2,14 +2,28 @@ namespace GildedRose.Tests;
 
 public class ProgramTests
 {
-    IList<Item> _items;
     Program _program;
+    IList<Item> _items;
 
     public ProgramTests()
     {
         _program = new Program();
-        _items = new List<Item>();
+        _program.Items = new List<Item>();
+        _items = _program.Items;
     }
 
+    [Fact]
+    public void aged_brie_increases_quality_as_days_pass() 
+    {
+      _items.Add(new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 });
+
+      Item cheese = _items[0];
+
+      Assert.Equal(0, cheese.Quality);
+
+      _program.UpdateQuality();
+
+      Assert.Equal(1, cheese.Quality);
+    }
 
 }

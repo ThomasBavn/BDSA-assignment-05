9bbb4a4e8a88dc1bd35f2a97732f5fd826cc14dd
Lauge Milling <lm@comonnetwork.com>
2022-10-07 13:05:30 +0200

    test constructor

--- GildedRose.Tests/ProgramTests.cs
+++ GildedRose.Tests/ProgramTests.cs
@@ -2,6 +2,13 @@ namespace GildedRose.Tests;
 
 public class ProgramTests
 {
+    IList < Item > Items;
+    Program p;
+
+    public ProgramTests() {
+      p = new Program();
+      Items = new List<Item>();
+    }
     [Fact]
     public void TestTheTruth()
     {

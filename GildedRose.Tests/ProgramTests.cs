2947024c10455f3a1bc9562f0710737a3fd01173
Johan <jric@itu.dk>
2022-10-07 13:32:11 +0200

    Sulfuras test done

--- GildedRose.Tests/ProgramTests.cs
+++ GildedRose.Tests/ProgramTests.cs
@@ -13,17 +13,39 @@ public class ProgramTests
     }
 
     [Fact]
-    public void aged_brie_increases_quality_as_days_pass() 
+    public void Sulfuras_Will_NEVER_age() 
     {
-      _items.Add(new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 });
+      _items.Add(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 });
+      _items.Add(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 });
 
-      Item cheese = _items[0];
+      Item Sulfuras = _items[0];
+      Item Sulfuras2ElectricBoogaloo = _items[1];
 
-      Assert.Equal(0, cheese.Quality);
+      Assert.Equal(0, Sulfuras.SellIn);
+      Assert.Equal(-1, Sulfuras2ElectricBoogaloo.SellIn);
 
       _program.UpdateQuality();
 
-      Assert.Equal(1, cheese.Quality);
+      Assert.Equal(0, Sulfuras.SellIn);
+      Assert.Equal(-1, Sulfuras2ElectricBoogaloo.SellIn);
+    }
+
+    [Fact]
+    public void Sulfuras_Will_NEVER_degrade() 
+    {
+      _items.Add(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 });
+      _items.Add(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 });
+
+      Item Sulfuras = _items[0];
+      Item Sulfuras2ElectricBoogaloo = _items[1];
+
+      Assert.Equal(80, Sulfuras.Quality);
+      Assert.Equal(80, Sulfuras2ElectricBoogaloo.Quality);
+
+      _program.UpdateQuality();
+
+      Assert.Equal(80, Sulfuras.Quality);
+      Assert.Equal(80, Sulfuras2ElectricBoogaloo.Quality);
     }
 
 }

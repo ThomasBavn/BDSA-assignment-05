ddea14d3571de348fa03e8bad04112c9988b1b79
Johan <jric@itu.dk>
2022-10-07 14:05:31 +0200

    new main weee

--- GildedRose.Tests/ProgramTests.cs
+++ GildedRose.Tests/ProgramTests.cs
@@ -182,4 +182,42 @@ public class ProgramTests
       Assert.Equal(0, concertInFive.Quality);
     }
 
+      [Fact]
+    public void after_due_date_degrade_twice_as_fast()
+    {
+        //Arrange
+        _items.Add(new Item { Name = "A new item", SellIn = -2, Quality = 10 });
+
+        //Act
+        _program.UpdateQuality();
+        var updatedItem = _items[0];
+        var expected = new Item { Name = "A new item", SellIn = -3, Quality = 8 };
+        //Assert
+
+        Assert.Equal(8,updatedItem.Quality);
+    }
+
+
+
+    [Fact]
+    public void quality_never_under_zero()
+    {
+        //Arrange
+        _items.Add(new Item { Name = "A new item", SellIn = -2, Quality = 1 });
+        _items.Add(new Item { Name = "Another item", SellIn = 1, Quality = 0 });
+        _items.Add(new Item { Name = "Third item", SellIn = -1, Quality = 2 });
+
+
+        _program.UpdateQuality();
+        var expected = new List<Item>(new[] {
+            new Item { Name = "A new item", SellIn = -3, Quality = 0 },
+            new Item { Name = "Another item", SellIn = 0, Quality = 0 },
+            new Item { Name = "Third item", SellIn = -2, Quality = 0 }
+        });
+
+        for(int i = 0; i < expected.Count(); i++){
+            Assert.Equal(expected[i].Quality,_items[i].Quality);
+        }
+    }
+
 }

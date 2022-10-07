0ba0b6b1bdafb41710843563b584fa6f27975ef2
Thomas Sørup Bavnhøj <tbav@itu.dk>
2022-10-07 14:12:32 +0200

    Test cases by Lauge and Johan

--- GildedRose.Tests/ProgramTests.cs
+++ GildedRose.Tests/ProgramTests.cs
@@ -64,5 +64,165 @@ public class ProgramTests
         }
     }
 
+    [Fact]
+    public void any_item_does_not_exceed_50_in_quality_as_days_pass() 
+    {
+      _items = new List < Item > {
+        new Item {
+          Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20
+        },
+        new Item {
+          Name = "Aged Brie", SellIn = 2, Quality = 0
+        },
+        new Item {
+          Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7
+        },
+        new Item {
+          Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80
+        },
+        new Item {
+          Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80
+        },
+        new Item {
+          Name = "Backstage passes to a TAFKAL80ETC concert",
+          SellIn = 15,
+          Quality = 20
+        },
+        new Item {
+          Name = "Backstage passes to a TAFKAL80ETC concert",
+          SellIn = 10,
+          Quality = 49
+        },
+        new Item {
+          Name = "Backstage passes to a TAFKAL80ETC concert",
+          SellIn = 5,
+          Quality = 49
+        },
+        new Item {
+          Name = "Conjured Mana Cake", SellIn = 3, Quality = 6
+        }
+      };
+
+      for(int i = 0; i < 31; i++) {
+        _program.UpdateQuality();
+        foreach(Item item in _items) {
+          bool below50 = item.Quality < 50;
+          if(item.Name.Equals("Sulfuras, Hand of Ragnaros")) {
+            Assert.False(below50);
+          } else {
+            Assert.True(below50);
+          }
+        }
+      }
+    }
+
+    [Fact]
+    public void Sulfuras_Will_NEVER_age() 
+    {
+      _items.Add(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 });
+      _items.Add(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 });
+
+      Item Sulfuras = _items[0];
+      Item Sulfuras2ElectricBoogaloo = _items[1];
+
+      Assert.Equal(0, Sulfuras.SellIn);
+      Assert.Equal(-1, Sulfuras2ElectricBoogaloo.SellIn);
+
+      _program.UpdateQuality();
+
+      Assert.Equal(0, Sulfuras.SellIn);
+      Assert.Equal(-1, Sulfuras2ElectricBoogaloo.SellIn);
+    }
+
+    [Fact]
+    public void Sulfuras_Will_NEVER_degrade() 
+    {
+      _items.Add(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 });
+      _items.Add(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 });
+
+      Item Sulfuras = _items[0];
+      Item Sulfuras2ElectricBoogaloo = _items[1];
+
+      Assert.Equal(80, Sulfuras.Quality);
+      Assert.Equal(80, Sulfuras2ElectricBoogaloo.Quality);
+
+      _program.UpdateQuality();
+
+      Assert.Equal(80, Sulfuras.Quality);
+      Assert.Equal(80, Sulfuras2ElectricBoogaloo.Quality);
+    }
+
+    [Fact]
+    public void BackstagePasses_will_increase_by_1_when_more_than_10_days_left() 
+    {
+      _items.Add(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 20 });
+
+      Item concertInFifteen = _items[0];
+
+      Assert.Equal(20, concertInFifteen.Quality);
+
+      _program.UpdateQuality();
+
+      Assert.Equal(23, concertInFifteen.Quality);
+
+      _program.UpdateQuality();
+      _program.UpdateQuality();
+      _program.UpdateQuality();
+      _program.UpdateQuality();
+
+      Assert.Equal(35, concertInFifteen.Quality);
+
+      _program.UpdateQuality();
+
+      Assert.Equal(0, concertInFifteen.Quality);
+    }
+
+    [Fact]
+    public void BackstagePasses_will_increase_by_2_when_there_is_less_than_ten_days_left() 
+    {
+      _items.Add(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 20 });
+
+      Item concertInTen = _items[0];
+
+      Assert.Equal(20, concertInTen.Quality);
+
+      _program.UpdateQuality();
+
+      Assert.Equal(22, concertInTen.Quality);
+
+      _program.UpdateQuality();
+      _program.UpdateQuality();
+      _program.UpdateQuality();
+      _program.UpdateQuality();
+
+      Assert.Equal(30, concertInTen.Quality);
+
+      _program.UpdateQuality();
+
+      Assert.Equal(33, concertInTen.Quality);
+    }
+
+    [Fact]
+    public void BackstagePasses_will_increase_Quality_by_3_when_less_than_five_days_left_but_become_worthless_after_the_sellby_Becomes_less_than_zero() 
+    {
+      _items.Add(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 20 });
+      Item concertInFive = _items[0];
+      
+      Assert.Equal(20, concertInFive.Quality);
+
+      _program.UpdateQuality();
+
+      Assert.Equal(23, concertInFive.Quality);
+
+      _program.UpdateQuality();
+      _program.UpdateQuality();
+      _program.UpdateQuality();
+      _program.UpdateQuality();
+      Assert.Equal(35, concertInFive.Quality);
+
+      _program.UpdateQuality();
+      Assert.Equal(0, concertInFive.Quality);
+    }
+
 
 }
